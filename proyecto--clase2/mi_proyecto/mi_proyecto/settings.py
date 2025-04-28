a7df25e489490ab3144e466dac55b0666651dcff
Valenn31 <101520377+Valenn31@users.noreply.github.com>
2025-04-28 15:18:35 -0300

    act

--- proyecto--clase2/mi_proyecto/mi_proyecto/settings.py
+++ proyecto--clase2/mi_proyecto/mi_proyecto/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2m6nl8g!%qt2!+kqwoyhz+e97&my(_a@2qii-vtx(y_**$dt9f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
